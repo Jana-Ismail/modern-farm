5f24e59ab6381d94016baa304ad9ae47a2ec826f
Jana Ismail <jana.ismail.nss@gmail.com>
2025-02-26 01:42:57 -0600

    Defined plantSeeds() function in tractor.js module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,10 @@
-import { createAsparagus } from "./asparagus.js";
-import { createCorn } from "./corn.js";
-import { addPlant, usePlants } from "./field.js";
+import { usePlants } from "./field.js";
 import { createPlan } from "./plan.js"
-import { createPotato } from "./potato.js";
-import { createSoybean } from "./soybean.js";
-import { createSunflower } from "./sunflower.js";
+import { plantSeeds } from "./tractor.js";
+
 
 const yearlyPlan = createPlan();
-const asparagusSeed = createAsparagus();
-const cornSeed = createCorn();
-const potatoSeed = createPotato();
-const soybeanSeed = createSoybean();
-const sunflowerSeed = createSunflower();
+plantSeeds(yearlyPlan)
+console.log(usePlants())
 
-addPlant(asparagusSeed);
-addPlant(cornSeed);
-const plants = usePlants();
-console.log(plants)
-
-console.log("Welcome to the main module")
-console.log(asparagusSeed)
-console.log(cornSeed)
-console.log(potatoSeed)
-console.log(soybeanSeed)
-console.log(sunflowerSeed)
 
