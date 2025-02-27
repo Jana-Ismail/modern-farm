f2cd6232d11dc218d54f7319b2395db7ac880216
Jana Ismail <jana.ismail.nss@gmail.com>
2025-02-26 22:09:42 -0600

    Updated logic in harvester.js to output one food type with total number of harvested plants of that type | updated Catalog function to render html in a table format | updated CSS

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,13 +1,43 @@
-export const Catalog = (harvestedPlantsArr) => {
+// Optional function to return html to display each type of food
+// only once, and the total number of harvested plants for each food type
+export const Catalog = (outputTotalsObj) => {
     const container = document.querySelector(".container")
     
-    let html = ""
-    html += harvestedPlantsArr.map(plant => {
-        return `
-            <section class="plant">${plant.type}</section>
-        `
+    let html = `
+        <h2>Total Yearly Harvest Outputs</h2>
+        <table class="output-table">
+        <tr class="output-table-headers">
+            <th>Food</th>
+            <th>Quantity</th>
+        </tr>
+    `
+    for (const [key, value] of Object.entries(outputTotalsObj)) {
+        html += `
+            <tr>
+                <td class="food-type">${key}</td>
+                <td class="food-output">${value}</td>
+            </tr>
+            `
     }
-    ).join("")
+
+    html += `</table>`
 
     container.innerHTML += html
 }
+
+// Function to return html for each output plant as its own section/element
+// for each occurrence
+
+// export const Catalog = (harvestedPlantsArr) => {
+//     const container = document.querySelector(".container")
+    
+//     let html = ""
+//     html += harvestedPlantsArr.map(plant => {
+//         return `
+//             <section class="plant">${plant.type}</section>
+//         `
+//     }
+//     ).join("")
+
+//     container.innerHTML += html
+// }
--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,31 +1,51 @@
+const outputTotals = {
+    Asparagus: 0,
+    Corn: 0,
+    Potato: 0,
+    Soybean: 0,
+    Sunflower: 0,
+    Wheat: 0
+}
+
 export const harvestPlants = (plantsArr) => {
     const plantsOutput = []
     for (const plantObj of plantsArr) {
         let output = plantObj.output
+        const plantType = plantObj.type
 
-        if (plantObj.type === "Corn") {
+        if (plantType === "Corn") {
             output = plantObj.output / 2
         }
 
         for (let i = 0; i < output; i++) {
             plantsOutput.push(plantObj);
+            
+            outputTotals[`${plantType}`] += 1
         }
     }
 
-    plantsOutput.sort((a, b) => a.type.localeCompare(b.type))
+    // option to sort harvested plants output
+    // alphabetically by type
+        plantsOutput.sort((a, b) => a.type.localeCompare(b.type))
 
-    // plantsOutput.sort((a, b) => {
-    //     const typeA = a.type
-    //     const typeB = b.type
+        // ||
 
-    //     if (typeA < typeB) {
-    //         return -1;
-    //     }
-    //     if (typeA > typeB) {
-    //         return 1
-    //     }
-    //     return 0;
-    // });
+        // plantsOutput.sort((a, b) => {
+        //     const typeA = a.type
+        //     const typeB = b.type
 
+        //     if (typeA < typeB) {
+        //         return -1;
+        //     }
+        //     if (typeA > typeB) {
+        //         return 1
+        //     }
+        //     return 0;
+        // });
+    console.log(outputTotals)
     return plantsOutput
+}
+
+export const getYearlyOutputTotals = () => {
+    return {...outputTotals}
 }
